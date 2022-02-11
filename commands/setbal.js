0ace46b8707f2f1b228c3b70d5032e4a7220cab8
Owen <owencompher@outlook.com>
2022-02-11 15:28:53 -0500

    created set balance command

---
+++ commands/setbal.js
@@ -0,0 +1,17 @@
+module.exports = {
+    execute(arg) {
+        const int = arg.interaction;
+        const db = arg.database;
+        const member = int.options.get('user').member;
+        const amount = int.options.get('amount').value;
+        db.findOne({_id: member.user.id}, (err, userDoc) => {
+            if (userDoc) {
+                db.update({_id: member.id}, {$set: {bal: amount}}, {}, ()=>{});
+                int.reply({content: `Set ${member.displayName}'s balance to ${amount}`, ephemeral: true});
+            } else {
+                arg.interaction.reply({content: 'there was a problem getting the info from the database', ephemeral: true});
+                console.log('had an issue with getting info from database for user '+member.id);
+            }
+        })
+    }
+}
