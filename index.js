1ec647438cc7780b0ed08d9d82da5ed680424721
Owen <72040115+owencompher@users.noreply.github.com>
2022-02-25 19:04:51 +0000

    created dev branch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({intents:["GUILDS", "GUILD_MEMBERS"]});
-const token = require("./token.json")
+const token = process.env['TOKEN'];
 
 const fs = require("fs");
 client.commands = new Discord.Collection();
